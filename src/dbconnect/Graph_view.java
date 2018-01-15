bd0cc5e5c9de5af7f7983928b3a54f5228569553
50616017 <50616017@50616017-pc>
2018-01-15 11:57:12 +0900

    わからっへん

---
+++ src/dbconnect/Graph_view.java
@@ -0,0 +1,15 @@
+package dbconnect;
+
+public class Graph_view {
+
+	public void setBounds(int i, int j, int k, int l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setVisible(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
